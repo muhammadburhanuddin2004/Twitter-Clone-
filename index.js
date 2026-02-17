7b35239a52ddd3503bffbabd6c6801899988a6bc
Borhan uddin <muhammadborhanuddin68@gmail.com>
2026-02-17 13:06:12 +0500

    2nd commit - Content rendered & Icons added

--- index.js
+++ index.js
@@ -1,4 +1,47 @@
-import { tweetsData } from "../data.js";
+import { tweetsData } from "./data.js";
 
 
-console.log(tweetsData)
+const tweetInput = document.getElementById('tweet-input')
+const inputBtn = document.getElementById('tweet-btn')
+
+inputBtn.addEventListener('click', function () {
+    console.log(tweetInput.value)
+})
+
+function getFeedHtml() {
+    let FeedHtml = ''
+
+    tweetsData.forEach(function (tweet) {
+        FeedHtml += `
+        <div class="tweet">
+            <div class="tweet-inner">
+                <img src= ${tweet.profilePic} class="profile-pic">
+                    <div>
+                        <p class="handle"> ${tweet.handle}</p>
+                        <p class="tweet-text">${tweet.tweetText}</p>
+                        <div class="tweet-details">
+                            <span class="tweet-detail">
+                                <i class="fa-regular fa-comment-dots"></i>
+                                ${tweet.replies.length}
+                            </span>
+                            <span class="tweet-detail">
+                                <i class="fa-solid fa-heart"></i>
+                                ${tweet.likes}
+                            </span>
+                            <span class="tweet-detail">
+                                <i class="fa-solid fa-retweet"></i>
+                                ${tweet.retweets}
+                            </span>
+                        </div>
+                    </div>
+            </div>
+        </div>`
+    })
+    return FeedHtml
+}
+function render() {
+    document.getElementById("feed").innerHTML = getFeedHtml()
+
+}
+
+render()
